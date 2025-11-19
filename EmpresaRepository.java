ee95d52cd4bf58f0469c022b9e77c12078dba663
Daniela.torres <160885593+Danielatrres123@users.noreply.github.com>
2025-11-19 15:49:39 -0500

    Delete EmpresaRepository.java

--- EmpresaRepository.java
+++
@@ -1,7 +0,0 @@
-package com.bolsa.backend.repository;
-
-import com.bolsa.backend.model.Empresa;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface EmpresaRepository extends MongoRepository<Empresa, String> {
-}
